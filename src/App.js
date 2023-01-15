d9e0631b1e94a98e788523b219e7637d9c20d42f
Amit Kumar Thakur <kingston.amit9830@gmail.com>
2023-01-15 13:18:42 +0530

    setup complete for tailwind css and react router dom

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <div >
+      <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
     </div>
   );
 }
