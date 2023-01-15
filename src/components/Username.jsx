183dd03a8c7dbf172fc944aa16eeaee4e423e34a
Amit Kumar Thakur <kingston.amit9830@gmail.com>
2023-01-15 16:59:46 +0530

    completed username ui with error handling

--- src/components/Username.jsx
+++ src/components/Username.jsx
@@ -1,12 +1,31 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import style from '../styles/Username.module.css'
+import { Toaster } from 'react-hot-toast'
+// formik use for validate the userdata in form 
+import { useFormik } from 'formik';
+import { usernameValidate } from '../helper/validate'
+
+
 import avatar from '../assets/avatar.jpg'
 
-
 const Username = () => {
+
+  const formik = useFormik({
+    initialValues: {
+      username: ''
+    },
+    validate: usernameValidate,
+    validateOnBlur: false,
+    validateOnChange: false,
+    onSubmit: async values => {
+      console.log(values);
+    }
+  })
+
   return (
     <div className="container mx-auto">
+    <Toaster position='top-center' reverseOrder={false}></Toaster>
       <div className="flex justify-center items-center h-screen">
         <div className={style.glass}>
           <div className="title flex flex-col items-center">
@@ -15,12 +34,12 @@ const Username = () => {
               Welcome Back !!!
             </span>
           </div>
-          <form action="" className="py-1">
+          <form action="" className="py-1" onSubmit={formik.handleSubmit}>
             <div className="profile flex justify-center py-4">
-              <img  className={style.profile_img} src={avatar} alt="avater" />
+              <img className={style.profile_img} src={avatar} alt="avater" />
             </div>
             <div className="textbox flex flex-col items-center gap-6">
-              <input type="text" className={style.textbox} placeholder='Username' />
+              <input {...formik.getFieldProps('username')} type="text" className={style.textbox} placeholder='Username' />
               <button type='submit' className={style.btn}>Let's Go</button>
             </div>
 
