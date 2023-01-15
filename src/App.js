ddceae06ddb40ae80393d67d485f842302c640d0
Amit Kumar Thakur <kingston.amit9830@gmail.com>
2023-01-15 14:39:20 +0530

    componenets and routes setup completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,52 @@
+import {createBrowserRouter,RouterProvider} from 'react-router-dom'
+
+// import all components
+import PageNotFound from './components/PageNotFound';
+import Password from './components/Password';
+import Profile from './components/Profile';
+import Recovery from './components/Recovery';
+import Register from './components/Register';
+import Reset from './components/Reset';
+import Username from './components/Username';
+
+
+// root routes
+const router = createBrowserRouter([
+  {
+    path : '/',
+    element : <Username />
+  },
+  {
+    path : '/password',
+    element : <Password />
+  },
+  {
+    path : '/register',
+    element : <Register />
+  },
+  {
+    path : '/recovery',
+    element : <Recovery />
+  },
+  {
+    path : '/profile',
+    element : <Profile />
+  },
+  {
+    path : '/reset',
+    element : <Reset />
+  },
+  {
+    path : '*',
+    element : <PageNotFound />
+  },
+])
 
 function App() {
   return (
-    <div >
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    </div>
+    <main >
+        <RouterProvider router={router}></RouterProvider> 
+    </main>
   );
 }
 
---
+++ src/components/Username.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Username = () => {
+  return (
+    <div>Username</div>
+  )
+}
+
+export default Username
