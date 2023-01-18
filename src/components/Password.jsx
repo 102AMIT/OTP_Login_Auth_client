8c8f2b2776e2cc3f89deeb9b3dad15a5500a88a5
Amit Kumar Thakur <kingston.amit9830@gmail.com>
2023-01-18 14:51:53 +0530

    password completed successfully

--- src/components/Password.jsx
+++ src/components/Password.jsx
@@ -1,17 +1,19 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import style from '../styles/Username.module.css'
-import { Toaster } from 'react-hot-toast'
+import toast, { Toaster } from 'react-hot-toast'
 // formik use for validate the userdata in form 
 import { useFormik } from 'formik';
 import { passwordValidate } from '../helper/validate'
 import useFetch from '../hooks/fetch.hook';
 import { useAuthStore } from '../store/store'
-
+import { verifyPassword } from '../helper/helper'
 import avatar from '../assets/avatar.jpg'
 
 
 const Password = () => {
+
+  const navigate = useNavigate();
   const { username } = useAuthStore(state => state.auth);
   const [{ isLoading, apiData, serverError }] = useFetch(`/user/${username}`);
 
@@ -23,7 +25,18 @@ const Password = () => {
     validateOnBlur: false,
     validateOnChange: false,
     onSubmit: async values => {
-      console.log(values);
+      let loginPromise = verifyPassword({ username, password: values.password })
+      toast.promise(loginPromise, {
+        loading: 'Checking...',
+        success: <b>Login Successfully...</b>,
+        error: <b>Password Not Match..!</b>
+      });
+
+      loginPromise.then(res => {
+        let { token } = res.data;
+        localStorage.setItem('token', token);
+        navigate('/profile');
+      })
     }
   })
 
