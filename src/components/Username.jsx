1ef67ff0b94c1b001541b396483a3c821ef9e56f
Amit Kumar Thakur <kingston.amit9830@gmail.com>
2023-01-20 21:48:07 +0530

    css updated for mobile view

--- src/components/Username.jsx
+++ src/components/Username.jsx
@@ -36,7 +36,7 @@ const Username = () => {
       <Toaster position='top-center' reverseOrder={false}></Toaster>
       <div className="flex justify-center items-center h-screen">
         <div className={style.glass}>
-          <div className="title flex flex-col items-center">
+          <div className="title flex flex-col items-center justify-center">
             <h4 className='text-5xl font-bold'>Hello..!</h4>
             <span className="py-4 text-xl w-2/3 text-center text-gray-500">
               Welcome Back !!!
